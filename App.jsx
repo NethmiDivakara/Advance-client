291ca68a325bedbfdbb20e214824478e111ca163
Nethmi Divakara <Nethmidivakara@gmail.com>
2025-12-18 23:42:50 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from './components/NavBar';
+import PropertyList from './components/PropertyList';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <PropertyList />
+    </>
+  );
+}
+
+export default App;
